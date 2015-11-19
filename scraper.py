e1441aa290666523e197415486d43b9932bd9e55
Joseph Catrambone <jo.jcat@gmail.com>
2015-11-19 18:54:01 -0500

    Fix tiny bugs in scraper.

--- scraper.py
+++ scraper.py
@@ -7,72 +7,95 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 
-def download_page(url, target_width, target_height, crop=False, embed=True, file_prefix="", encoding="png"):
+def download_page(url, target_width, target_height, crop=False, pad=True, file_prefix="", target_extension="jpg", encoding="JPEG", image_links=True, image_embeds=False):
 	# If crop = True, we cut into a section of the image after rescaling.
-	# If embed = True, we pad the image on either side.
-	if not crop ^ embed:
-		print("WARNING: Need crop or rescale.")
+	# If pad = True, we pad the image on either side.
+	print("Getting URL {}".format(url))
 	result = requests.get(url)
 	soup = BeautifulSoup(result.content, 'html.parser')
 	# Get image URL
+	print("Found {} links.".format(len(soup.find_all('a'))))
 	index = 0
-	for link in soup.find_all('a'):
-		# Pull URL from hyperlink
-		if link.get('href').endswith(".png"):
+	if image_links:
+		for link in soup.find_all('a'):
+			# Pull URL from hyperlink
 			img_src = link.get('href')
-		else:
-			continue
-		# Fix the head.
-		if img_src.startswith("/"):
-			img_src = img_src[1:]
-		if img_src.startswith("/"):
-			img_src = img_src[1:] # Some sites start with "//blah"
-		if not (img_src.startswith("http://") or img_src.startswith("https://"):
-			img_src = "http://" + img_src
-		# Download image
-		try:
-			result2 = requests.get(img_src)
-		except MissingSchema:
-			continue # Bad parse.  Retry URL?
-		# Download image
-		img = None
-		try:
-			img = Image.open(BytesIO(result2.content))
-		except IOError:
-			continue
-		# Write image after resize.
-		w = float(img.size[0])
-		h = float(img.size[1])
-		newimg = None
-		if embed: # Pad the outside of the image.
-			# Calculate new size
-			max_res = max(w, h)
-			new_width = target_width*float(w/max_res)
-			new_height = target_height*float(h/max_res)
-			# Center image in new image.
-			newimg = Image.new(img.mode, (target_width, target_height))
-			offset_x = (target_width/2)-(new_width/2)
-			offset_y = (target_height/2)-(new_height/2)
-			box = (offset_x, offset_y, offset_x+new_width, offset_y+new_height)
-			newimg.paste(img.resize((new_width, new_height))
-		elif crop: # Cut a section from the middle of the image.
-			# Calculate size
-			res_cap = min(w, h)
-			new_width = target_width*(w/float(res_cap))
-			new_height = target_height*(h/float(res_cap))
-			# Cut image chunk.
-			offset_x = (new_width/2)-(target_width/2)
-			offset_y = (new_height/2)-(target_height/2)
-			newimg = img.resize(
-				(new_width, new_height)
-			).crop(
-				(offset_x, offset_y, offset_x+target_width, offset_y+target_height)
-			)
-		else: # Just write it.
-			newimg = img
-		# Find a name.
-		filename = file_prefix + index
-		while os.path.isfile(filename):
-			index += 1
-		newimg.save(filename, format=encoding)
-		print("Wrote file {}".format(filename))
+			if img_src is not None and img_src.endswith(target_extension):
+				index = get_image(img_src, target_width, target_height, crop, pad, file_prefix, encoding, index)
+	if image_embeds:
+		for link in soup.find_all('img'):
+			img_src = link.get('src')
+			if img_src is not None and img_src.endswith(target_extension):
+				index = get_image(img_src, target_width, target_height, crop, pad, file_prefix, encoding, index)
+
+def get_image(img_src, target_width, target_height, crop, pad, file_prefix, target_extension, index):
+	print("Getting {}".format(img_src))
+	# Fix the head.
+	if img_src.startswith("/"):
+		img_src = img_src[1:]
+	if img_src.startswith("/"):
+		img_src = img_src[1:] # Some sites start with "//blah"
+	if not (img_src.startswith("http://") or img_src.startswith("https://")):
+		img_src = "http://" + img_src
+	# Download image
+	try:
+		result2 = requests.get(img_src)
+	except MissingSchema:
+		return index # Bad parse.  Retry URL?
+	# Download image
+	img = None
+	try:
+		img = Image.open(BytesIO(result2.content))
+	except IOError:
+		return index
+	# Write image after resize.
+	w = float(img.size[0])
+	h = float(img.size[1])
+	newimg = None
+	if pad: # Pad the outside of the image.
+		# Calculate new size
+		max_res = max(w, h)
+		new_width = int(target_width*float(w/max_res))
+		new_height = int(target_height*float(h/max_res))
+		# Center image in new image.
+		newimg = Image.new(img.mode, (target_width, target_height))
+		offset_x = int((target_width/2)-(new_width/2))
+		offset_y = int((target_height/2)-(new_height/2))
+		box = (offset_x, offset_y, offset_x+new_width, offset_y+new_height)
+		newimg.paste(img.resize((new_width, new_height)), box)
+	elif crop: # Cut a section from the middle of the image.
+		# Calculate size
+		res_cap = min(w, h)
+		new_width = int(target_width*(w/float(res_cap)))
+		new_height = int(target_height*(h/float(res_cap)))
+		# Cut image chunk.
+		offset_x = int((new_width/2)-(target_width/2))
+		offset_y = int((new_height/2)-(target_height/2))
+		newimg = img.resize(
+			(new_width, new_height)
+		).crop(
+			(offset_x, offset_y, offset_x+target_width, offset_y+target_height)
+		)
+	else: # Just write it.
+		newimg = img
+	# Find a name.
+	filename = file_prefix + str(index) + "." + target_extension
+	while os.path.isfile(filename):
+		index += 1
+		filename = file_prefix + str(index) + "." + target_extension
+	newimg.save(filename)
+	print("Wrote file {}".format(filename))
+	return index+1
+
+def main():
+	while True:
+		url = raw_input("URL: ")
+		if not url:
+			return False;
+		prefix = raw_input("Prefix: ")
+		embedded = (raw_input("Embedded images (y/[n]): ") == 'y')
+		linked = (raw_input("Linked images (y/[n]): ") == 'y')
+		download_page(url, 256, 256, pad=True, encoding="JPEG", image_links=linked, image_embeds=embedded, file_prefix=prefix)
+
+if __name__=="__main__":
+	main()
